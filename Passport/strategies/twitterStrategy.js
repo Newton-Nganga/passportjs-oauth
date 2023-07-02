529e31630f42f098e705949e97659b8ece9a1043
Newton-Nganga <nganga7newt@gmail.com>
2023-07-02 22:54:07 +0300

    resolved the twitter credentials error

--- Passport/strategies/twitterStrategy.js
+++ Passport/strategies/twitterStrategy.js
@@ -1,17 +1,17 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// const TwitterStrategy = require("passport-twitter").Strategy;
+const TwitterStrategy = require("passport-twitter").Strategy;
 
-// const twitter = new TwitterStrategy({
-//     customerKey: process.env.TWITTER_CUSTOMER_KEY,
-//     customerSecret: process.env.TWITTER_CUSTOMER_SECRET,
-//     callbackURL: `http://localhost:${process.env.PORT}/twitter/callback`,
-//   },
-//   (token, tokenSecret, profile, cb) => {
-//     console.log("call");
-//     //   done(null,profile)
-//     process.nextTick(() => console.log("Twitter profile:", profile));
-//   }
-// );
+const twitter = new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CUSTOMER_KEY,
+    consumerSecret: process.env.TWITTER_CUSTOMER_SECRET,
+    callbackURL: `http://localhost:${process.env.PORT}/twitter/callback`,
+  },
+  (accessToken,refreshToken, tokenSecret, profile, cb) => {
+    console.log("call");
+    //   done(null,profile)
+    process.nextTick(() => console.log("Twitter profile:", profile));
+  }
+);
 
-// module.exports = twitter;
+module.exports = twitter;
