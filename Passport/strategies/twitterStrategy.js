d720243f722ef4958f143fa077d9c41cb3b92fc6
Newton-Nganga <nganga7newt@gmail.com>
2023-07-05 09:29:37 +0300

    Reconfigured twitter-strategy urls

--- Passport/strategies/twitterStrategy.js
+++ Passport/strategies/twitterStrategy.js
@@ -5,12 +5,18 @@ const TwitterStrategy = require("passport-twitter").Strategy;
 const twitter = new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: `http://localhost:${process.env.PORT}/twitter/callback`,
-  },
-  (accessToken,refreshToken, tokenSecret, profile, cb) => {
-    console.log("call");
-    //   done(null,profile)
-    process.nextTick(() => console.log("Twitter profile:", profile));
+    callbackURL: `http://localhost:${process.env.PORT}/api/twitter/callback`,
+  },async(accessToken,refreshToken, tokenSecret, profile, done) => {
+    const user = User.findOne({twitterIdId:profile.id})
+    if(!user){
+      const newUser = newUser({
+        twitterId:profile.id,
+        email:profile.email ? profile.email : "",
+        username:profile.username,
+      })
+      await newUser.save()
+    }
+    return done(null,profile)
   }
 );
 
