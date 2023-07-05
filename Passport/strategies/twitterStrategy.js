e4fe88158c1f5ff1e7f4a3749f1905eb9437231d
Newton-Nganga <nganga7newt@gmail.com>
2023-07-05 11:31:11 +0300

    twitter credentials updated

--- Passport/strategies/twitterStrategy.js
+++ Passport/strategies/twitterStrategy.js
@@ -2,21 +2,25 @@ require("dotenv").config();
 
 const TwitterStrategy = require("passport-twitter").Strategy;
 
-const twitter = new TwitterStrategy({
+const twitter = new TwitterStrategy(
+  {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: `${process.env.URL}/api/twitter/callback`,
-  },async(accessToken,refreshToken, tokenSecret, profile, done) => {
-    const user = User.findOne({twitterIdId:profile.id})
-    if(!user){
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+   callbackURL: `${process.env.URL}/api/twitter/callback`,
+  },
+  async (accessToken, refreshToken, tokenSecret, profile, done) => {
+    const user = User.findOne({ twitterIdId: profile.id });
+    if (!user) {
       const newUser = newUser({
-        twitterId:profile.id,
-        email:profile.email ? profile.email : "",
-        username:profile.username,
-      })
-      await newUser.save()
+        twitterId: profile.id,
+        email: profile.email ? profile.email : "",
+        username: profile.username,
+      });
+      await newUser.save();
     }
-    return done(null,profile)
+    return done(null, profile);
   }
 );
 
